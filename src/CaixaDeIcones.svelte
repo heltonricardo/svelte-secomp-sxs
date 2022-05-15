99498269efa7f78c4062ca4698a9e6577f601c4b
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-05-14 22:39:52 -0300

    Estilo nos icones de rede social (CaixaDeIcones)

--- src/CaixaDeIcones.svelte
+++ src/CaixaDeIcones.svelte
@@ -1,18 +1,18 @@
 <div class="icones">
     <a target="_blank" href="mailto:heltonricardo@proton.me">
-        <i class="fa fa-envelope-o" aria-hidden="true" />
+        <i class="icone fa fa-envelope-o" aria-hidden="true" />
     </a>
 
     <a target="_blank" href="https://instagram.com/helton.x">
-        <i class="fa fa-instagram" aria-hidden="true" />
+        <i class="icone fa fa-instagram" aria-hidden="true" />
     </a>
 
     <a target="_blank" href="https://linkedin.com/in/heltonricardo">
-        <i class="fa fa-linkedin" aria-hidden="true" />
+        <i class="icone fa fa-linkedin" aria-hidden="true" />
     </a>
 
     <a target="_blank" href="https://github.com/heltonricardo">
-        <i class="fa fa-github-alt" aria-hidden="true" />
+        <i class="icone fa fa-github-alt" aria-hidden="true" />
     </a>
 </div>
 
@@ -20,5 +20,16 @@
     .icones {
         display: flex;
         justify-content: center;
+    }
+
+    .icone {
+        color: white;
+        width: 30px;
+        width: 30px;
+        padding: 10px;
+        border: 1px solid white;
+        border-radius: 50%;
+        font-size: 28px;
+        margin: 5px;
     }
 </style>
