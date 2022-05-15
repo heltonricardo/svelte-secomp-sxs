116332daf6084ab964d3686a57d8edf3813dcd12
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-05-15 15:25:46 -0300

    Adicionando mais texto em tags html no slot (App)

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,14 @@
 
 <Capa />
 
-<Secao titulo="Seção 1">
-	Passando dados via slot para componente filho!
+<Secao titulo="Sobre Mim">
+	<p>
+		Sou analista e desenvolvedor de softwares e um entusiasta do mundo da
+		tecnologia.
+	</p>
+	<p>
+		Trabalho no Inatel - Instituto Nacional de Telecomunicações - usando
+		tecnologias como Java, Spring e Hibernate. Nas horas vagas, gosto de me
+		arriscar no frontend usando JavaScript, React e Svelte.
+	</p>
 </Secao>
