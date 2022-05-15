9485cb960ed5d3caac13ff79f961dbe542fde05a
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-05-14 22:36:43 -0300

    Substituindo link por icone (index, CaixaDeIcones)

--- src/CaixaDeIcones.svelte
+++ src/CaixaDeIcones.svelte
@@ -1,8 +1,19 @@
 <div class="icones">
-    <a target="_blank" href="mailto:heltonricardo@proton.me">E-mail</a>
-    <a target="_blank" href="https://instagram.com/helton.x">Instagram</a>
-    <a target="_blank" href="https://linkedin.com/in/heltonricardo">LinkedIn</a>
-    <a target="_blank" href="https://github.com/heltonricardo">GitHub</a>
+    <a target="_blank" href="mailto:heltonricardo@proton.me">
+        <i class="fa fa-envelope-o" aria-hidden="true" />
+    </a>
+
+    <a target="_blank" href="https://instagram.com/helton.x">
+        <i class="fa fa-instagram" aria-hidden="true" />
+    </a>
+
+    <a target="_blank" href="https://linkedin.com/in/heltonricardo">
+        <i class="fa fa-linkedin" aria-hidden="true" />
+    </a>
+
+    <a target="_blank" href="https://github.com/heltonricardo">
+        <i class="fa fa-github-alt" aria-hidden="true" />
+    </a>
 </div>
 
 <style>
