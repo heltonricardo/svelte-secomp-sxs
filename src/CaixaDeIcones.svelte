25f38654494b20575928a8a7bff26e000336c512
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-05-14 22:32:28 -0300

    Adicionando links para redes sociais (CaixaDeIcones)

---
+++ src/CaixaDeIcones.svelte
@@ -0,0 +1,13 @@
+<div class="icones">
+    <a target="_blank" href="mailto:heltonricardo@proton.me">E-mail</a>
+    <a target="_blank" href="https://instagram.com/helton.x">Instagram</a>
+    <a target="_blank" href="https://linkedin.com/in/heltonricardo">LinkedIn</a>
+    <a target="_blank" href="https://github.com/heltonricardo">GitHub</a>
+</div>
+
+<style>
+    .icones {
+        display: flex;
+        justify-content: center;
+    }
+</style>
