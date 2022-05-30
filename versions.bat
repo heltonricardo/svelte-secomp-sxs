96467102d4df7311ee6c09f7bd360e61fe3ddbd0
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-05-30 08:26:24 -0300

    Update versions.bat

--- versions.bat
+++ versions.bat
@@ -1,9 +1,12 @@
 @echo off
+title Svelte Secomp IFSP-CJO 2022 - Passo a passo
+
 git pull -q
-for /f "tokens=1,*" %%s in ('git log --reverse --oneline') do (
-  git pull -q
-  git reset -q --hard %%s
-  echo %%t
-  echo.
-  pause > nul
+
+for /f "tokens=1,*" %%c in ('git log --reverse --oneline') do (
+    git pull -q
+    git reset -q --hard %%c
+    echo %%d
+    echo.
+    pause > nul
 )
